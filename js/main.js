1fe59f21c2cc11e294ab8e204200114d4a8111c8
Danz <DvRossen@outlook.com>
2023-01-20 17:27:33 +0100

    update 1.1.0-a

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 window.addEventListener("load", init)
+window.addEventListener( "load", checkTimeHours);
 
-
+// console.log("main")
 function init(){
  addEventListener('click',checkBoxClickHandler);
+
+// console.log("init")
 }
 
-// yes I wrote this entire script for a singular joke
-function checkBoxClickHandler(e){
-let clickedItem = e.target;
-if(clickedItem.classList.contains('funkyLilCheckbox')){
-    window.alert("There is however JavaScript code :)");
+// yes I wrote this entire script for a singular joke/ checkbox pop-up
+let boxClicked = false;
+function checkBoxClickHandler(e) {
+        let clickedItem = e.target;
+        if (clickedItem.id.includes("funkyLilCheckbox")) {
+            boxClicked = !boxClicked;
+          if(boxClicked){  window.alert("data stolen :p");}
+        }
+    console.log(boxClicked);
 }
+
+function checkTimeHours(){
+    let date = new Date();
+    let ToD = date.getHours();
+    console.log(ToD);
 }
